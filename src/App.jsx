2e200c7298439ebdad32e951ff198a10e3e22eb7
Nenegodova <timofeevap63@gmail.com>
2026-05-20 00:20:30 +0300

    update checklist

--- src/App.jsx
+++ src/App.jsx
@@ -217,17 +217,16 @@ const ui = {
   },
 
   card: {
-  display: "flex",
-  alignItems: "flex-start",
-  gap: 10,
-  padding: 14,
-  border: `1px solid ${border}`,
-  background: card,
-  textAlign: "left",
-  borderRadius: 12,
-  transition: "all 0.18s ease",
-  cursor: "pointer",
-},
+    display: "flex",
+    alignItems: "flex-start",
+    gap: 10,
+    padding: 14,
+    border: `1px solid ${border}`,
+    background: card,
+    textAlign: "left",
+    borderRadius: 12,
+    transition: "all 0.15s ease"
+  },
 
   taskText: {
     flex: 1,
